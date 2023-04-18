4e68f1c6756d794dc4a6134825019f9102912ba0
uwla <uwlalinux@gmail.com>
2023-04-18 09:12:52 -0300

    enabling adding/deleting tags to Grupo

--- app/Models/Grupo.php
+++ app/Models/Grupo.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Uwla\Ltags\Traits\Taggable as HasTags;
+use Uwla\Ltags\Contracts\Taggable as TaggableContract;
 
-class Grupo extends Model
+class Grupo extends Model implements TaggableContract
 {
-    use HasFactory;
+    use HasFactory, HasTags;
 }
