e8736564e7270498555912a7d16b941ba6753403
uwla <uwlalinux@gmail.com>
2023-04-19 09:55:44 -0300

    add user seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([GrupoSeeder::class]);
+        $this->call([
+            GrupoSeeder::class,
+            UserSeeder::class
+        ]);
     }
 }
