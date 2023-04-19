add12c32f111fac4abefbe9e6f950a06f4fa4e8a
uwla <uwlalinux@gmail.com>
2023-04-19 09:57:03 -0300

    add api crud routes for user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GrupoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('grupo', GrupoController::class);
+Route::apiResource('user', UserController::class);
