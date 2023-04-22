3c59f82392550f25d4612c52e0353edb27a0e1a7
uwla <uwlalinux@gmail.com>
2023-04-22 09:29:17 -0300

    return groups along with their tags

--- app/Models/Grupo.php
+++ app/Models/Grupo.php
@@ -10,4 +10,13 @@ use Uwla\Ltags\Contracts\Taggable as TaggableContract;
 class Grupo extends Model implements TaggableContract
 {
     use HasFactory, HasTags;
+
+    public static function all($columns = ['*'])
+    {
+        $all = parent::all($columns);
+        $tagged = self::withTags($all);
+        foreach ($tagged as $g)
+            $g->tags = $g->tags->pluck('name');
+        return $tagged;
+    }
 }
