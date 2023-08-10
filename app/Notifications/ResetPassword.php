b51ee995d6c02dc5b7497c1d7f097cd71b759893
uwla <uwlalinux@gmail.com>
2023-08-10 10:27:07 -0300

    customize reset password email

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -5,6 +5,7 @@ namespace App\Notifications;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class ResetPassword extends ResetPasswordNotification implements ShouldQueue
 {
@@ -18,5 +19,18 @@ class ResetPassword extends ResetPasswordNotification implements ShouldQueue
         $frontend = env('FRONTEND_URL');
         $url = "{$frontend}/conta/resetar-senha?token={$token}&email={$email}";
         return $url;
+    }
+
+    /**
+     * Get the verify email notification mail message for the given URL.
+     *
+     * @param  string  $url
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    protected function buildMailMessage($url)
+    {
+        return (new MailMessage)
+            ->subject('Resetar senha')
+            ->markdown('emails.reset', ['url' => $url]);
     }
 }
---
+++ resources/views/emails/reset.blade.php
@@ -0,0 +1,11 @@
+Para resetar sua senha, clique no link abaixo:
+
+<x-mail::button :url="$url">
+RESETAR SENHA
+</x-mail::button>
+
+Se não funcionar, copie e cole o seguinte link no seu navegador:
+
+<br/><br/>
+
+{{ "$url" }}
