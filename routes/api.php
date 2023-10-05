c04b40ed54a0fa67af19260eee401c11e94beac8
uwla <uwlalinux@gmail.com>
2023-10-05 10:41:36 -0300

    add name to nameless routes

--- routes/api.php
+++ routes/api.php
@@ -48,15 +48,15 @@ Route::group($attributes['auth'], function () {
     Route::group(['middleware' => 'guest'], function () {
         Route::post('register', 'register')->name('register');
         Route::post('login', 'login')->name('login');
-        Route::post('login/admin', 'loginAdmin');
+        Route::post('login/admin', 'loginAdmin')->name('login-admin');
     });
 
-    Route::post('logout', 'logout')->middleware('auth:sanctum');
+    Route::post('logout', 'logout')->middleware('auth:sanctum')->name('logout');
 });
 
 Route::get('/verified', function() {
     return ['verified' => 'ok!'];
-})->middleware(['auth:sanctum', 'verified']);
+})->middleware(['auth:sanctum', 'verified'])->name('account.verified');
 
 // ─────────────────────────────────────────────────────────────────────────────
 // account routes
@@ -64,7 +64,8 @@ Route::get('/verified', function() {
 Route::group($attributes['account'], function () {
     // request link to verify account
     Route::post('verify_link', 'sendVerificationEmail')
-        ->middleware('guest');
+        ->middleware('guest')
+        ->name('verification.link');
 
    // verify account
     Route::get('verify/{id}/{hash}', 'verifyEmail')
@@ -73,17 +74,17 @@ Route::group($attributes['account'], function () {
 
     // profile
     Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
-        Route::get('/bookmarks', 'bookmarks');
-        Route::get('/comments', 'comments');
-        Route::get('/grupos', 'grupos');
-        Route::get('/votes', 'votes');
-        Route::get('/profile', 'getProfile');
-        Route::post('/profile', 'updateProfile');
+        Route::get('/bookmarks', 'bookmarks')->name('account.bookmarks');
+        Route::get('/comments', 'comments')->name('account.comments');
+        Route::get('/grupos', 'grupos')->name('account.grupos');
+        Route::get('/votes', 'votes')->name('account.votes');
+        Route::get('/profile', 'getProfile')->name('account.profile');
+        Route::post('/profile', 'updateProfile')->name('account.profile-update');
     });
 
     // password resets
     Route::group(['middleware' => 'guest', 'prefix' => 'password'], function () {
-        Route::post('/link', 'sendResetLinkEmail');
+        Route::post('/link', 'sendResetLinkEmail')->name('password.reset-email');
         Route::post('/reset', 'resetPassword')->name('password.reset');
     });
 });
@@ -92,7 +93,7 @@ Route::group($attributes['account'], function () {
 // resource routes
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::get('/permission', [PermissionController::class, 'index']);
+    Route::apiResource('/permission', PermissionController::class, ['only' => 'index']);
     Route::apiResource('/grupo', GrupoController::class);
     Route::apiResource('/user', UserController::class);
     Route::apiResource('/role', RoleController::class);
@@ -102,16 +103,16 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::apiResource('/comment', CommentController::class, ['except' => ['index', 'show']]);
 
     // bookmarks follow a different logic
-    Route::post('/bookmark/{grupo}', [BookmarkController::class, 'store']);
-    Route::delete('/bookmark/{grupo}', [BookmarkController::class, 'destroy']);
+    Route::post('/bookmark/{grupo}', [BookmarkController::class, 'store'])->name('bookmark.store');
+    Route::delete('/bookmark/{grupo}', [BookmarkController::class, 'destroy'])->name('bookmark.destroy');
 });
 
 // ─────────────────────────────────────────────────────────────────────────────
 // public routes
 
 Route::group($attributes['public'], function () {
-    Route::get('/tags', 'tags');
-    Route::get('/grupos', 'grupos');
-    Route::get('/grupos/{grupo}', 'grupo');
-    Route::get('/categorias', 'categories');
+    Route::get('/tags', 'tags')->name('public.tags');
+    Route::get('/grupos', 'grupos')->name('public.grupo.index');
+    Route::get('/grupos/{grupo}', 'grupo')->name('public.grupo.show');
+    Route::get('/categorias', 'categories')->name('public.category');
 });
